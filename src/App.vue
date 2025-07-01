776ebbee6536e2b083cb8b8a593f04f10e80d14b
247023 Michał Hołyński <247023@edu.p.lodz.pl>
2025-07-01 20:38:37 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <UiShowcase />
+  </div>
+</template>
+
+<script setup>
+import UiShowcase from './components/UiShowcase.vue'
+</script>
+
+<style scoped>
+.container {
+  padding: 2rem;
+  font-family: sans-serif;
+}
+</style>
---
+++ src/components/UiShowcase.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Welcome to the Vue Starter Template 🚀</h1>
+    <button @click="clicked = !clicked">Toggle</button>
+    <p v-if="clicked">🎉 You clicked it!</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const clicked = ref(false)
+</script>
+
+<style scoped>
+button {
+  margin-top: 1rem;
+  padding: 0.5rem 1rem;
+  font-size: 1rem;
+}
+</style>
